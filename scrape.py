a6ce30195ca71b89ac1e616d4f2babc7fa44f327
Nathan <side_nathan@hotmail.com>
2023-05-22 16:21:38 -0400

    Change parser to parse quotes instead of links
    Also made parser output to a quotes.txt file.

--- scrape.py
+++ scrape.py
@@ -8,6 +8,9 @@ response = requests.get(url)
 # Create BeautifulSoup object
 soup = BeautifulSoup(response.content, "html.parser")
 
-links = soup.find_all("a")
-for link in links:
-    print(link.get("href"))
+spans = soup.find_all("span", class_ = "text")
+
+with open('./output-files/quotes.txt', 'w', encoding = 'utf-8', errors = 'replace') as file:
+    for span in spans:
+        modified_text = span.text.replace("�", '\"')
+        file.write(modified_text + '\n')
